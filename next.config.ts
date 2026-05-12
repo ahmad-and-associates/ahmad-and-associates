7605c31491e56fe6725c2291ae52ad15b5e4ac23
Ahmad And Associates <nadiyasiddique983@gmail.com>
2026-05-12 15:35:46 +0530

    prod fix 4

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
+
 import type { NextConfig } from "next";
 
-const basePath = (process.env.NEXT_PUBLIC_BASE_PATH || "").trim();
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? "/ahmad-and-associates" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
